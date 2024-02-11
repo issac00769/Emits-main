db2917b7e9076dc7f30b517c655851a6250b98cf
issac00769 <70919258+issac00769@users.noreply.github.com>
2024-02-10 23:16:39 -0500

    cd

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <header class="menu-header">
-      <nav>
-        <template v-for="(item, index) in listamenu" :key="item.path">
-          <RouterLink :to="item.path" :class="{ 'unclickable': index % 2 !== 0 || index == 0, 'active': $route.path === item.path }">
-            {{ item.name }}
-          </RouterLink>
-        </template>
-      </nav>
-    </header>
-  </div>
+  <header class="menu-header">
+    <nav>
+      <template v-for="(item, index) in listamenu" :key="item.path">
+        <RouterLink :to="item.path" :class="{ 'unclickable': index % 2 !== 0 || index == 0, 'active': $route.path === item.path }">
+          {{ item.name }}
+        </RouterLink>
+      </template>
+    </nav>
+  </header>
 
   <main>
     <RouterView />
@@ -26,42 +24,42 @@ const listamenu = router.options.routes
 <style scoped>
 .menu-header {
   width: 100%;
-  background-color: #34495e; /* Cambiar color de fondo */
-  padding: 1rem; 
+  background-color: #34495e;
+  padding: 1rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  position: fixed; /* Fijar la posición del encabezado */
-  top: 0; /* Colocar en la parte superior */
+  position: fixed;
+  top: 0;
   left: 0;
+  z-index: 1000; /* Ajustar z-index para asegurar que esté por encima de otros elementos */
 }
 
 nav {
   display: flex;
-  padding: 0.5rem 10rem; 
-
+  padding: 0.5rem 10rem;
 }
 
 nav a {
-  padding: 0.5rem 1rem; 
+  padding: 0.5rem 1rem;
   text-decoration: none;
-  color: #fff; /* Cambiar color del texto */
-  font-weight: bold; 
-  text-align: center; /* Alinear texto al centro */
-  margin: 0.5rem 0; /* Espacio entre elementos */
-  border-radius: 5px; /* Agregar bordes redondeados */
-  transition: background-color 0.3s ease; /* Agregar transición al cambio de color de fondo */
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
+  margin: 0.5rem 0;
+  border-radius: 50px;
+  transition: background-color 0.3s ease;
 }
 
 nav a:hover {
-  background-color: #000000; /* Cambiar color de fondo al pasar el mouse */
+  background-color: #000000;
   color: #41b883;
 }
 
 .unclickable {
-  pointer-events: none; 
+  pointer-events: none;
   color: #999;
 }
 
 .active {
-  background-color: #41b883; /* Cambiar color de fondo del enlace activo */
+  background-color: #41b883;
 }
 </style>
---
+++ src/components/ImagenCondicionada.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <button @click="toggleImagen" class="toggle-button">{{ mostrarImagen ? 'Ocultar' : 'Mostrar' }} Imagen</button>
+      <img v-if="mostrarImagen" 
+           src="https://th.bing.com/th/id/R.09e3221fed188d060e7636f475dd1e0e?rik=tuXhJnV%2b46pkSg&pid=ImgRaw&r=0" 
+           alt="Imagen condicional" 
+           class="half-screen-image" />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const mostrarImagen = ref(true);
+  
+  const toggleImagen = () => {
+    mostrarImagen.value = !mostrarImagen.value;
+  };
+  </script>
+  
+  <style scoped>
+  
+  .toggle-button {
+    position: relative;
+    top: 2rem;
+    left: 12rem;
+    height: 3rem;
+    width: 25rem;
+    z-index: 1001; 
+    padding: 0.5rem 1rem;
+    background-color: #41b883; 
+    color: #fff; 
+    border: none;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    border: 2px solid rgb(0, 0, 0);
+
+    
+  }
+  
+  .toggle-button:hover {
+    background-color: #34495e; 
+    border: 2px solid rgb(255, 255, 255);
+  }
+  
+  .half-screen-image {
+    width: 100%; 
+    height: calc(100vh - 4rem); 
+    position: fixed;
+    top: 4rem; 
+    right: 0;
+    transform: translateX(50%);
+    object-fit: cover; 
+    z-index: 999; 
+    opacity: 0.5;
+  }
+  </style>
+  
